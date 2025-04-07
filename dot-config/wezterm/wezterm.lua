8a30c31efdeb79a0075804f7dfedb3fec333071c
qriaa <urbaniak.g11@gmail.com>
2025-04-07 15:22:00 +0200

    Add wezterm config

---
+++ dot-config/wezterm/wezterm.lua
@@ -0,0 +1,34 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+local config = wezterm.config_builder()
+
+config.font = wezterm.font('Inconsolata Nerd Font')
+config.font_size = 11
+
+config.use_fancy_tab_bar = false
+
+local wez_mod = 'CTRL|SHIFT'
+config.keys = {
+    { mods = wez_mod, key = 'p', action = act.ActivateCommandPalette },
+
+    { mods = wez_mod, key = 'h', action = act.ActivateTabRelative(-1) },
+    { mods = wez_mod, key = 'j', action = act.ActivatePaneDirection('Prev') },
+    { mods = wez_mod, key = 'k', action = act.ActivatePaneDirection('Next') },
+    { mods = wez_mod, key = 'l', action = act.ActivateTabRelative(1) },
+
+    { mods = wez_mod, key = 'n', action = act.SplitPane({
+        direction = 'Down',
+        size = { Percent = 30 }
+    })},
+    { mods = wez_mod, key = 'm', action = act.SplitPane({
+        direction = 'Right',
+        size = { Percent = 50 }
+    })},
+
+    { mods = wez_mod, key = 't', action = act.SpawnTab('CurrentPaneDomain') },
+
+    { mods = wez_mod, key = 'q', action = act.CloseCurrentPane( { confirm = true } ) },
+    { mods = wez_mod, key = 'w', action = act.CloseCurrentTab( { confirm = true } ) },
+}
+
+return config
