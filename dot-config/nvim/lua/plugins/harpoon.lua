4f1a147dd930f9703e3bf3abc59237e8008ca74b
qriaa <urbaniak.g11@gmail.com>
2025-10-05 16:31:16 +0200

    Add buffer support for telescope, add harpoon keymaps

--- dot-config/nvim/lua/plugins/harpoon.lua
+++ dot-config/nvim/lua/plugins/harpoon.lua
@@ -1,20 +1,47 @@
+local utils = require("config.utils")
 local maps = {
-    a = { lhs = "<leader>a", mode = "n", desc = "Add to harpoon (harpoon)" },
-    Ce = { lhs = "<C-e>", mode = "n", desc = "Open harpoon list (harpoon)" },
+    add = { lhs = "<leader>a", mode = "n", desc = "Add to harpoon (harpoon)" },
+    open_list = { lhs = "<C-e>", mode = "n", desc = "Open harpoon list (harpoon)" },
+    select_1 = { lhs = "<C-1>", mode = "n", desc = "Select 1. harpoon (harpoon)" },
+    select_2 = { lhs = "<C-2>", mode = "n", desc = "Select 2. harpoon (harpoon)" },
+    select_3 = { lhs = "<C-3>", mode = "n", desc = "Select 3. harpoon (harpoon)" },
+    select_4 = { lhs = "<C-4>", mode = "n", desc = "Select 4. harpoon (harpoon)" },
 }
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-        { maps.a.lhs, desc = maps.a.desc },
-        { maps.Ce.lhs, desc = maps.Ce.desc },
-    },
+    dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim' },
+    keys = utils.maps_to_keys(maps),
     config = function(plugin, opts)
         local harpoon = require("harpoon")
         harpoon.setup(opts)
 
-        vim.keymap.set(maps.a.mode, maps.a.lhs, function() harpoon:list():add() end)
-        vim.keymap.set(maps.Ce.mode, maps.Ce.lhs, function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        tscope_conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer = tscope_conf.file_previewer({}),
+                sorter = tscope_conf.generic_sorter({}),
+                -- attach_mappings = function(_, map)
+                --     map("i", "")
+                -- end,
+            }):find()
+        end
+
+        maps.add.rhs = function() harpoon:list():add() end
+        maps.open_list.rhs = function() toggle_telescope(harpoon:list()) end
+        maps.select_1.rhs = function() harpoon:list():select(1) end
+        maps.select_2.rhs = function() harpoon:list():select(2) end
+        maps.select_3.rhs = function() harpoon:list():select(3) end
+        maps.select_4.rhs = function() harpoon:list():select(4) end
+        utils.keybind_maps(maps)
     end,
 }
--- dot-config/nvim/lua/plugins/telescope.lua
+++ dot-config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ local maps = {
     find_files = { lhs = "<leader>ff", mode = "n", desc = "Find files (telescope)" },
     find_grep = { lhs = "<leader>fg", mode = "n", desc = "Grep files (telescope)" },
     git_find = { lhs = "<leader>gf", mode = "n", desc = "Git files (telescope)" },
+    buffers = { lhs = "<leader>bl", mode = "n", desc = "List buffers (telescope)"},
 }
 return {
     'nvim-telescope/telescope.nvim',
@@ -10,12 +11,28 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = utils.maps_to_keys(maps),
     config = function(plugin, opts)
-        require("telescope").setup(opts)
         local builtin = require("telescope.builtin")
+        local actions = require("telescope.actions")
+        local opts = {
+            pickers = {
+                buffers = {
+                    mappings = {
+                        n = {
+                            ["d"] = actions.delete_buffer + actions.move_to_top
+                        },
+                        i = {
+                            ["<C-d>"] = actions.delete_buffer + actions.move_to_top
+                        },
+                    },
+                },
+            },
+        }
+        require("telescope").setup(opts)
 
         maps.find_files.rhs = builtin.find_files
         maps.find_grep.rhs = builtin.live_grep
         maps.git_find.rhs = builtin.git_files
+        maps.buffers.rhs = builtin.buffers
         utils.keybind_maps(maps)
     end,
 }
