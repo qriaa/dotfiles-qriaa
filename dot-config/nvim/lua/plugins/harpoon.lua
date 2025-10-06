a5401605793c9d6599eb0308b2eb029d476c9465
qriaa <urbaniak.g11@gmail.com>
2025-10-06 18:08:02 +0200

    Remove telescope integration from harpoon (this would be a separate proj)

--- dot-config/nvim/lua/plugins/harpoon.lua
+++ dot-config/nvim/lua/plugins/harpoon.lua
@@ -10,34 +10,14 @@ local maps = {
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim' },
+    dependencies = { "nvim-lua/plenary.nvim" },
     keys = utils.maps_to_keys(maps),
     config = function(plugin, opts)
         local harpoon = require("harpoon")
         harpoon.setup(opts)
 
-        tscope_conf = require("telescope.config").values
-        local function toggle_telescope(harpoon_files)
-            local file_paths = {}
-            for _, item in ipairs(harpoon_files.items) do
-                table.insert(file_paths, item.value)
-            end
-
-            require("telescope.pickers").new({}, {
-                prompt_title = "Harpoon",
-                finder = require("telescope.finders").new_table({
-                    results = file_paths,
-                }),
-                previewer = tscope_conf.file_previewer({}),
-                sorter = tscope_conf.generic_sorter({}),
-                -- attach_mappings = function(_, map)
-                --     map("i", "")
-                -- end,
-            }):find()
-        end
-
         maps.add.rhs = function() harpoon:list():add() end
-        maps.open_list.rhs = function() toggle_telescope(harpoon:list()) end
+        maps.open_list.rhs = function() harpoon.ui:toggle_quick_menu(harpoon:list()) end
         maps.select_1.rhs = function() harpoon:list():select(1) end
         maps.select_2.rhs = function() harpoon:list():select(2) end
         maps.select_3.rhs = function() harpoon:list():select(3) end
