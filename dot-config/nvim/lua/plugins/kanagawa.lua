1c2b7da97b56c4eef37fcad9b762d3e528f983e0
qriaa <urbaniak.g11@gmail.com>
2025-10-16 00:28:42 +0200

    Add virt-column, make color scheme look better

--- dot-config/nvim/lua/plugins/kanagawa.lua
+++ dot-config/nvim/lua/plugins/kanagawa.lua
@@ -3,10 +3,24 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-        theme = "dragon"
+        theme = "dragon",
+        colors = {
+            theme = {
+                all = {
+                    ui = {
+                        bg_gutter = "none",
+                    },
+                },
+            },
+        },
+        overrides = function(colors)
+            return {
+                CursorLine = { bg = colors.theme.ui.bg_m1 },
+            }
+        end,
     },
     config = function(plugin, opts)
         require("kanagawa").setup(opts)
-    vim.cmd("colorscheme kanagawa-dragon")
+        vim.cmd("colorscheme kanagawa-dragon")
     end
 }
