d06f7d5afcb37d3204f76e23b0216d6c45a10636
qriaa <urbaniak.g11@gmail.com>
2025-10-16 21:49:32 +0200

    Make gitsigns work

--- dot-config/nvim/lua/plugins/gitsigns.lua
+++ dot-config/nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,16 @@
+local utils = require("config.utils")
+local maps = {
+    blame = { lhs = "<leader>gb", mode = "n", desc = "Git blame (gitsigns)" },
+}
 return {
     "lewis6991/gitsigns.nvim",
-    event = "User FilePost",
+    event = {"BufReadPost", "BufNewFile", "BufWritePre"},
     opts = {},
+    keys = utils.maps_to_keys(maps),
     config = function(plugin, opts)
-        require("gitsigns").setup(opts)
+        local gitsigns = require("gitsigns")
+        gitsigns.setup(opts)
+        maps.blame.rhs = function() gitsigns.blame_line({ full = true }) end
+        utils.keybind_maps(maps)
     end,
 }
