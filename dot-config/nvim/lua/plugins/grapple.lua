62cbbb92fdc1e3c2cca4702e153dd3f7f8e7bce5
qriaa <urbaniak.g11@gmail.com>
2025-11-05 12:39:42 +0100

    Add grapple 5 hotkey

--- dot-config/nvim/lua/plugins/grapple.lua
+++ dot-config/nvim/lua/plugins/grapple.lua
@@ -6,6 +6,7 @@ local maps = {
     select_2 = { lhs = "<C-2>", mode = "n", desc = "Select 2. grapple (grapple)" },
     select_3 = { lhs = "<C-3>", mode = "n", desc = "Select 3. grapple (grapple)" },
     select_4 = { lhs = "<C-4>", mode = "n", desc = "Select 4. grapple (grapple)" },
+    select_5 = { lhs = "<C-5>", mode = "n", desc = "Select 5. grapple (grapple)" },
 }
 return {
     "cbochs/grapple.nvim",
@@ -24,6 +25,7 @@ return {
         maps.select_2.rhs = function() grapple.select({ index = 2 }) end
         maps.select_3.rhs = function() grapple.select({ index = 3 }) end
         maps.select_4.rhs = function() grapple.select({ index = 4 }) end
+        maps.select_5.rhs = function() grapple.select({ index = 5 }) end
         utils.keybind_maps(maps)
     end,
 }
