911eebe506720b2d7f96de3d2ac65ed823db41bc
qriaa <urbaniak.g11@gmail.com>
2025-11-05 19:13:23 +0100

    Use oh-my-posh with initial config

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -2,6 +2,9 @@ set -gx EDITOR 'nvim'
 set -gx PAGER 'less'
 set -g fish_greeting
 
+# --- XDG VARS ---
+test -n "$XDG_CONFIG_HOME"; or set -gx XDG_CONFIG_HOME "$HOME/.config/"
+
 # --- SOURCES ---
 source ~/.config/fish/git.fish
 
@@ -17,6 +20,8 @@ abbr -a la "lsd -lah"
 abbr -a lat "lsd -lah --tree"
 # terraform
 abbr -a tf "terraform"
+# kubectl
+abbr -a k "kubectl"
 # !!
 function last_history_item
     echo $history[1]
@@ -27,4 +32,14 @@ abbr -a !! --position anywhere --function last_history_item
 # starship init fish | source
 
 # --- oh-my-posh ---
-oh-my-posh init fish | source
+oh-my-posh init fish --config "$XDG_CONFIG_HOME/fish/qriaa.omp.yaml" | source
+
+function rerender_on_bind_mode_change --on-variable fish_bind_mode
+    if test "$fish_bind_mode" != paste -a "$fish_bind_mode" != "$FISH__BIND_MODE"
+        set -gx FISH__BIND_MODE $fish_bind_mode
+        omp_repaint_prompt
+    end
+end
+function fish_default_mode_prompt --description "Display vi prompt mode"
+    # This function is masked and does nothing
+end
